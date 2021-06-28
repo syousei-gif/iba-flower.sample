17f6814fbe6837bbfdd02f361ec259cd7cd0d24c
syousei-gif <77610482+syousei-gif@users.noreply.github.com>
2021-06-28 09:53:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ setInterval(function(){
   var bgImage = $('.first').css('backgroundImage');
 
   if(bgImage.lastIndexOf("/20.jpg") >-1){
-    $('.first').css('background-image', 'url(../flower/22.jpg)');
+    $('.first').css('background-image', 'url(22.jpg)');
     $('.first h1').html('敬う');
     $('.first p').html('ー大切な人への”変わらぬ”想いー');
   }
   else if(bgImage.lastIndexOf("/22.jpg") >-1){
-    $('.first').css('background-image', 'url(../flower/05.jpg)');
+    $('.first').css('background-image', 'url(05.jpg)');
     $('.first h1').html('彩る');
     $('.first p').html('ー花を愛でる心の余裕ー');
   }
  else if(bgImage.lastIndexOf("/05.jpg") >-1){
-    $('.first').css('background-image', 'url(../flower/20.jpg)');
+    $('.first').css('background-image', 'url(20.jpg)');
     $('.first h1').html('祝う');
     $('.first p').html('ー大切な人を大切に扱う生き方ー');
   }
